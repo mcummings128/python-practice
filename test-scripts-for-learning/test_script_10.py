d8e5a401e440424f6e70bd608216c8468ef53c64
Matt C <mcumm64@gmail.com>
2025-11-21 00:34:34 -0500

    ts_10: idiocy corrections

--- test-scripts-for-learning/test_script_10.py
+++ test-scripts-for-learning/test_script_10.py
@@ -6,12 +6,12 @@ from bs4 import BeautifulSoup
 def main():
     url = "https://www.gov.uk/search/news-and-communications"
     page = requests.get(url)
-    soup = BeautifulSoup(page,'html.parses')
+    soup = BeautifulSoup(page.content,'html.parser')
 
     # Inspecting article descriptions show they are <p> elements with the class name gem-c-document-list__item-description
-    descriptions = soup.find_all('p', class_='gem-c-document-list__item-description')
+    descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
 
-    for description in descriptions
+    for description in descriptions:
         print(description.string)
 
 if __name__ == "__main__":
