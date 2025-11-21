b5b5a1ca58e2c9e10fbdf30ee86bfd16437a49da
Matt C <mcumm64@gmail.com>
2025-11-21 01:17:41 -0500

    ts_11 logic scaff

--- test-scripts-for-learning/test_scripts_11/test_script_11.py
+++ test-scripts-for-learning/test_scripts_11/test_script_11.py
@@ -17,11 +17,10 @@
 # Once you have completed the exercise, you can run the following command in the VS code terminal  pytest tests.py
 
 import requests
-from bs4 import BeautifulSoup4
+from bs4 import BeautifulSoup
 
-# import html from index.html
+# import html from index.html by opening it, then reading its contents (like Bash 'cat; command sort of.)
 
-page = 
 
 # 'open' gets a file and returns it as a file object
 # 'with' helps ensure the file is properly closed when the block is exited (in leiu for something like file.close()) 
@@ -37,5 +36,39 @@ title = soup.title
 
 h1_text = soup.h1.string
 
-print(title)
-print(h1_text)
+product_list = soup.find("ul")
+# print(f"product_list = {product_list}")
+
+products = soup.find_all("li")
+product_names = []
+product_prices = []
+product_descriptions = []
+
+
+for product in products:
+    # findChildren is deprecated in Python 3.0.0 but for learning purposes is the easiest to use out of the box. It will get the children of the specified element. recursive=False means only the direct children
+    children_tags = product.findChildren(recursive=False)
+    product_name = product.find("h2")
+    product_names.append(product_name.string)
+    # Every price is in format of 'Price : €##' (## being a number). Use split() to delimit and take second piece, then strip trailing whitespace (rstrip()) (Actually rstrip() ended up not being needed but whatever)
+    product_price = product.find("p").string.split(": €")[1].rstrip()
+    print(f"product price = {product_price}")
+    product_prices.append(product_price)
+    product_description = children_tags[2]
+    print(f"description = {product_description}")
+    product_descriptions.append(product_description)
+    
+
+# product_names = []
+# name_elements = soup.find_all("h2")
+# for name_element in name_elements:
+    # product_names.append(name_element.string)
+
+product_prices = []
+name_elements = soup.find_all("h2")
+for name_element in name_elements:
+    product_prices.append(name_element.string)
+
+print(f"title = {title}")
+print(f"h1_text = {h1_text}")
+
