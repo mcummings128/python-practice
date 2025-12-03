5b9462ffa374385740aa02da1f3908a68c1835c5
Matt C <mcumm64@gmail.com>
2025-12-03 16:41:11 -0500

    Create generate_graph.py

---
+++ test-scripts-for-learning-2/manage-python-packages/generate_graph.py
@@ -0,0 +1,8 @@
+
+# imports the matplotlib.pyplot module as a var 'plt'.
+# matplotlib.pyplot is used to help create visualizations based on data, like charts, plots, etc.
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot([1, 2, 3, 4, 5])
+plt.show()
