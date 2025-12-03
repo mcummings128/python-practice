d819fa450752d0b2bc818fac4ca7a9094e51cbe0
Matt C <mcumm64@gmail.com>
2025-12-03 17:35:21 -0500

    Update number_rounding.py

--- test-scripts-for-learning-2/import-python-packages-and-modules/number_rounding.py
+++ test-scripts-for-learning-2/import-python-packages-and-modules/number_rounding.py
@@ -1,5 +1,6 @@
 import numpy
 
-print(numpy.ceil(1.2))
-print(numpy.ceil(1.933))
-print(numpy.floor(1.2))
+
+print(f"1.2 rounded up to the nearest int is  {numpy.ceil(1.2)}")
+print(f"1.933 rounded up to the nearest int is  {numpy.ceil(1.933)}")
+print(f"1.2 rounded down to the nearest int is {numpy.floor(1.2)}")
