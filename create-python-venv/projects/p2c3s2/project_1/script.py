bf22991f971f3f7cb14a87cfde9ad01964a8ec7a
Matt C <mcumm64@gmail.com>
2025-12-11 00:28:00 -0500

    Update script.py

--- create-python-venv/projects/p2c3s2/project_1/script.py
+++ create-python-venv/projects/p2c3s2/project_1/script.py
@@ -1,5 +1,9 @@
+# Doesn't run b/c dateutil module not installed
+# After pip install python-dateutil this works
+# It's interesting to note that pip install dateutil doesn't work, but you can still reference dateutil in the
+# On the flip side, from python-dateutil doesn't work either
 from dateutil import parser
 
-
+# Take a string and parse into datetime object
 parsed_date = parser.parse('3rd January 2014')
 print('The type of the parsed date is {}'.format(type(parsed_date)))
