a30d6044934539d6cdcc7010b9ffbdfe5f42ac5e
Matt C <mcumm64@gmail.com>
2025-12-12 00:21:58 -0500

    demo.py: notes; requirements; fix typo

--- create-python-venv/projects/demo-app-3/demo.py
+++ create-python-venv/projects/demo-app-3/demo.py
@@ -1,3 +1,9 @@
+# This is a script someone external wrote and you want to also use it.
+# It has a requirements.txt so you can use that to install the packages needed
+# First create and then activate your virutal environment
+# Then install what's in requirements.txt viathe command
+# pip install -r requirements.txt
+
 import requests
 from bs4 import BeautifulSoup
 
