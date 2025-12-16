4d2f19c9dd72e16cd8bd12df072a518a4b12e72a
Matt C <mcumm64@gmail.com>
2025-12-15 23:33:58 -0500

    Update script.py

--- create-python-venv/projects/p2c5s3/task_2/script.py
+++ create-python-venv/projects/p2c5s3/task_2/script.py
@@ -1,5 +1,10 @@
-import nltk
+# Another script that has been made for you. From OpenClassrooms exercise "Manage Virtual Environments Using Requirements Files", Task 1
+# Same gist: make venv, activate, try to run, fails d/t ModuleNotFound: ntlk
+# BUT there's no requirements.txt file! So use this file to see what packages you need to install
 
+
+import nltk
+nltk.download('punkt_tab')
 
 sentence = 'Open Classrooms is an excellent online learning provider'
 tokens = nltk.word_tokenize(sentence)
